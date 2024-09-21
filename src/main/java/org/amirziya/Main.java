fae6f68df62fb0e7cf31b58c9fd51c7d73f4a53d
amirziyacode <amzdidi@gmail.com>
2024-09-21 10:23:28 +0330

    Add_view_on_terminal

--- src/main/java/org/amirziya/Main.java
+++ src/main/java/org/amirziya/Main.java
@@ -1,5 +1,33 @@
 package org.amirziya;
 
+import data.InternationMorse;
+import data.InternationMorseClas;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {}
+    final  static String MASSAGE = "Enter Your  Massage :";
+
+    public static void main(String[] args) {
+
+        // TODO: Start and run App with Scanner
+
+        InternationMorse internationMorse = new InternationMorseClas();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of option you want : \n 1-Encode \n 2-Decode");
+        int option = input.nextInt();
+
+        if(option == 1){
+            Scanner inputEncode = new Scanner(System.in);
+            System.out.println(MASSAGE);
+            String massage = inputEncode.nextLine();
+            System.out.println(internationMorse.Encode(massage));
+        }if(option == 2){
+            Scanner inputDecode = new Scanner(System.in);
+            System.out.println(MASSAGE);
+            String decodeMassage = inputDecode.nextLine();
+            System.out.println( internationMorse.Decode(decodeMassage));
+        }
+    }
 }
